dc3f091c9d948da9a78a9cd852cda23c0e89f79f
santhoshpuppala59 <162947047+santhoshpuppala59@users.noreply.github.com>
2024-03-11 15:27:47 +0530

    vpc update

--- Project-1/ec2.tf
+++ Project-1/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "my-ec2" {
+/*resource "aws_instance" "my-ec2" {
   ami                    = "ami-0d92749d46e71c34c"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
@@ -12,4 +12,5 @@ resource "aws_instance" "my-ec2" {
     echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF  
   vpc_security_group_ids = [aws_security_group.myvpc-sg.id]
-}
+}*/
+
--- Project-1/vpc.tf
+++ Project-1/vpc.tf
@@ -1,4 +1,4 @@
-# Resource-1: Create VPC
+################################ Resource-1: Create VPC #################################
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -11,30 +11,113 @@ resource "aws_vpc" "my_vpc" {
 }
 
 
-# Create a public subnet
+################# Create a public subnet ############################
 
 resource "aws_subnet" "vpc-dev-public-subnet-1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+    tags = {
+    Name        = "Public-subnet"
+    environment      = "Prod"
+    
+  }
 
 }
 
-# Resource-3: Internet Gateway
+resource "aws_subnet" "vpc-dev-public-subnet-2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+    tags = {
+    Name        = "Public-Subnet"
+    environment      = "Prod"
+    
+  }
+
+}
+################## Private subnet ###################
+resource "aws_subnet" "vpc-dev-private-subnet-1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+    tags = {
+    Name        = "Private-subnet"
+    environment      = "Prod"    
+  }
+}
+
+resource "aws_subnet" "vpc-dev-private-subnet-2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+    tags = {
+    Name        = "Private subnet"
+    environment      = "Prod"
+    
+  }
+
+
+}
+
+################ Resource-3: Internet Gateway###############################
 
 resource "aws_internet_gateway" "myvpc_igw" {
   vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "vpc-IGW"
+  }
 
 }
 
-# Resource-4: Create Route Table
+#####################Nat Gateway###########################
+
+resource "aws_eip" "nat-eip" {
+  vpc = true
+   tags = {
+      Name = "nat-eip"
+      }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat-eip.id
+  subnet_id     = aws_subnet.vpc-dev-public-subnet-1.id
+  tags = {
+      Name = "nat-gateway"
+      }
+}
+
+
+###################  Create Route Table Public##########
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.my_vpc.id
 
+   tags = {
+    Name = "public-route-table"
+  }
+
 }
 
+###################  Create Route Table Private ##########
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+   tags = {
+    Name = "private-route-table"
+  }
+
+}
+
+#############Public Route association################################
 # Resource-5: Create Route in Route Table for Internet Access
 resource "aws_route" "vpc-public-rt" {
   route_table_id         = aws_route_table.public_rt.id
@@ -44,10 +127,37 @@ resource "aws_route" "vpc-public-rt" {
 }
 
 # Resource-6: Associate the Route Table with the Subnet
-resource "aws_route_table_association" "vpc-rt-ass-pub" {
+resource "aws_route_table_association" "vpc-rt-ass-pub-1a" {
   route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.vpc-dev-public-subnet-1.id
 }
+
+resource "aws_route_table_association" "vpc-rt-ass-pub-1b" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.vpc-dev-public-subnet-2.id
+}
+
+
+
+
+############# Route for NAT Gateway###############################
+resource "aws_route" "private_internet_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat.id
+}
+
+# Route table associations for both Public & Private Subnets
+resource "aws_route_table_association" "vpc-rt-ass-pri-1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.vpc-dev-private-subnet-1.id
+}
+
+resource "aws_route_table_association" "vpc-rt-ass-pri-1b" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.vpc-dev-private-subnet-2.id
+}
+
 
 # Resource-7: Create Security Group
 
