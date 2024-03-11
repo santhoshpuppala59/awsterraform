1375d23cc228f45d93bffe5e734a200ee00cb1bc
santhoshpuppala59 <162947047+santhoshpuppala59@users.noreply.github.com>
2024-03-11 15:38:26 +0530

    vpc subnet update

--- Project-1/vpc.tf
+++ Project-1/vpc.tf
@@ -1,6 +1,6 @@
 ################################ Resource-1: Create VPC #################################
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -15,7 +15,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "vpc-dev-public-subnet-1" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.1.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -29,7 +29,7 @@ resource "aws_subnet" "vpc-dev-public-subnet-1" {
 
 resource "aws_subnet" "vpc-dev-public-subnet-2" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.1.3.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
@@ -43,7 +43,7 @@ resource "aws_subnet" "vpc-dev-public-subnet-2" {
 ################## Private subnet ###################
 resource "aws_subnet" "vpc-dev-private-subnet-1" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.1.2.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
